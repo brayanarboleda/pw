d0cd670923bdb399949049a7b1503f2bb5adbe51
brayanarboleda <89162585+brayanarboleda@users.noreply.github.com>
2021-11-12 19:57:00 -0500

    brayan

---
+++ src/config.js
@@ -0,0 +1,17 @@
+$(" #boton").click(()=>{
+
+    console.log("le acabo de dar click al boton");
+    
+    });
+$(" #cerrar").click(()=>{
+    console.log("acaba de cerrar el modal");
+});
+$(" #btnGroupDrop1").click(()=>{
+    console.log("acabas de abrir un boton desplegable");
+});
+$(" #cerar").click(()=>{
+    console.log("acaba de cerrar el segundo modal");
+});
+$(" #segundo").click(()=>{
+    console.log("acabas de abrir el segundo modal");
+});
